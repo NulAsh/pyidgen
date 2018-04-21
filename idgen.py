902fc2abd18b8861352208b297ab68cae534955e
NulAsh <32499449+NulAsh@users.noreply.github.com>
2018-04-22 01:05:05 +0300

    Add files via upload

---
+++ idgen.py
@@ -0,0 +1,184 @@
+﻿import zlib
+import random
+import datetime
+maxpop = 280728988
+woman_names = [
+    'Maria','Jane','Stacy','Merideth','Beth','Angela','Mary','Tiffany',
+    'Pamela','April','Louise','Virginia','Nicole','Carolyn','Julia','Trina',
+    'Robin','Karen','Susan','Kathy','Shirley','Renee','Olivia','Beth','Gladys',
+    'Dianne','Ethelene','Patricia','Anne','Kathleen','Carol','Kimberly',
+    'Marcia','Marsha','Sharon','Ella','Evelyn','Glenda','Katie','Kate','Ann',
+    'Dana','Shannon','Elizabeth','Lillian','Jo Ann','Karen','Edith','Jenny',
+    'Deborah','Elisa','Becky','Barbara','Blair','Cynthia','Debbie','Diane',
+    'Margaret','Racheal','Shari','Gail','Sarah','Cathy','Katrina','Marlene',
+    'Patricia','Lisa','Candace','Beatrice','Betty','Wendy','Denise','Rebecca',
+    'Arlene','Stephanie','Flora','Martha','Nancy','Tanya','Helen','Patty',
+    'Della','Ida','Lisa','Jeanne','Theresa','Vanessa','Rose','Benita','Lucy',
+    'Suzette','Jessica','Blair','Gina','Melissa','Nikki','Victoria',
+    'Priscilla','Catherine','Joan','Sue','Sonja','Stephanie','Cindy','Alison',
+    'Samantha','Kelly','Carroll','Lauren','Louis','Alicia','Sylvia','Jenn',
+    'Theodora','Leanne','Krissy','Belle','Mandy','Erin','Marylin']
+man_names = [
+    'Michael','Mike','Randy','Richard','Ronald','Samuel','Scott','Willie',
+    'Stanley','William','Burt','Bob','Steve','Jason','Dennis','Joe','Jim',
+    'John','Robert','Sidney','Walter','Keith','David','Gwen','Thomas','Bill',
+    'Don','James','Alfred','Andrew','Arnold','Bruce','Bryan','Clyde','Charles',
+    'Doug','Dwight','Dwayne','Edward','Eric','Chris','Mark','Harry','Jerry',
+    'Leroy','George','Samuel','Wesley','Terry','Jeff','Franklin','Melvin',
+    'Marcus','Rudy','Jimmy','Eddie','Lewis','Ralph','Kenneth','Phillip',
+    'Gregory','Howard','Geoff','Theo','Jeffrey','Paul','Cliff','Darrell',
+    'Christopher','Anthony','Chuck','Sam','Phil','Fredrick','Johnny','Hugh',
+    'Alvin','Alan','Buck','Bo','Dewitt','Tom','Tony','Lawrence','Roger','Marc',
+    'Cecil','Kevin','Maurice','Byron','Patrick','Johnie','Daniel','Edwin',
+    'Jesse','Leonard','Andy','Norman','Eugene','Jerrell']
+surnames = [
+    'Abbot','Abe','Abrams','Acker','Ackman','Adams','Adam','Adamson','Addison',
+    'Adkins','Adler','Allen','Alford','Allison','Alley','Allgood','Anderson',
+    'Amos','Applegate','Apple','Arnold','Ashby','Austin','Austin','Baker',
+    'Baldwin','Banks','Bannerman','Barber','Barfield','Barkley','Barnett',
+    'Barry','Basinger','Bays','Beck','Bell','Benjamin','Bennett','Bentley',
+    'Biggs','Binder','Blackburn','Blackwood','Blake','Bobbitt','Boggs',
+    'Bolden','Borden','Bosworth','Bowden','Bowen','Bowers','Bowman','Bradley',
+    'Brady','Briggs','Britt','Brown','Bullock','Burton','Byrd','Carlon',
+    'Carter','Casey','Cates','Caudle','Chambers','Chandler','Chapman','Chavis',
+    'Cheek','Clapp','Clark','Clarke','Clemmons','Cline','Cobb','Coble','Cole',
+    'Coleman','Collins','Combs','Compton','Conner','Connor','Conway','Cook',
+    'Cowan','Cox','Crawford','Crutchfield','Cunningham','Daniel','Darnell',
+    'Davis','Day','Dawson','Dixon','Douglas','Dunn','Edwards','Ellis',
+    'Ellison','Emmett','Erwin','Evans','Evenson','Everhart','Farlow','Fann',
+    'Ferguson','Fields','Fish','Fitzgerald','Flowers','Forrest','Foster',
+    'Fowler','Frazier','Free','Freeman','Frye','Fulk','Fulton','Furr',
+    'Galloway','Gardner','Gaulden','Gauley','Gerkin','Getters','Gibbs',
+    'Gibson','Gilbert','Gillett','Glass','Glenn','Glover','Goble','Godfrey',
+    'Goldston','Goldstein','Good','Gordon','Gorham','Gorrell','Gossett',
+    'Graham','Grant','Graves','Gray','Green','Garfield','Greene','Gregory',
+    'Gross','Gwynn','Hager','Haines','Hairston','Ham','Hamilton','Hammond',
+    'Hanks','Hampton','Hamrick','Harden','Hardy','Hare','Harper','Harrel',
+    'Harris','Harrison','Hart','Hatfield','Hayes','Haynes','Helms','Henderson',
+    'Hendricks','Henley','Henson','Hester','Hatt','Hickman','Hicks','Higgins',
+    'Hill','Hines','Hinshaw','Hinson','Hodges','Hobson','Holder','Holl',
+    'Holiday','Holloway','Holt','Hoover','Hopkins','Horn','Hoskins','Hubbard',
+    'Hudson','Huff','Huffman','Hughes','Huneycutt','Hurst','Hutchers','Hylton',
+    'Irel','Irwin','Isley','Jaby','Jackson','Jacob','Jarrell','Jester',
+    'Johnson','Jones','Jordan','Joseph','Joyner','Justice','Keith','Kelley',
+    'Kellis','Kendall','Kennedy','Kenny','Kim','Kimble','King','Kinsey',
+    'Kirkman','Kiser','Knight','Knott','Lamb','Lambert','Lambeth','Lacey',
+    'Lane','Lang','Langley','Lankford','Lashley','Lawson','Leary','Leggett',
+    'Legrant','Lemonds','Lanahan','Leonard','Lewis','Lineback','Lindsay',
+    'Little','Lloyd','Livengood','Locke','Long','Lord','Love','Lovett','Lucas',
+    'Lowe','Lyons','Mabe','Mackey','Maddox','Maness','Mangum','Mann','Manning',
+    'Marion','Marks','Marsh','Marshall','Martin','Martinez','Mathews','Mathis',
+    'Matthews','May','Mayer','McClain','McConnell','McCormick','McCraw',
+    'McCullough','McDougald','McPherson','Medlin','Melvin','Mentis','Meyer',
+    'Middleton','Miller','Mitchell','Montgomery','Moore','Morrison','Morrow',
+    'Morton','Moss','Murphy','Murray','Neal','Newnam','Newton','Nichols',
+    'Nixon','Norris','Norton','Nunn','O''Brien','O''Donnel','Oldham','Olds',
+    'Oliver','Olson','O''Neal','Osborne','Owens','Padgett','Pace','Page',
+    'Painter','Parker','Parrish','Parsons','Patel','Patterson','Peacock',
+    'Pennington','Perkins','Peterson','Phillips','Pickard','Pickett','Piper',
+    'Pitts','Pleasants','Poole','Pope','Posey','Potter','Powers','Prendergast',
+    'Pressley','Preston','Priddy','Pugh','Purcell','Purvis','Putnam','Quick',
+    'Raines','Ramsey','Rankin','Reese','Reeves','Reid','Richards','Rich',
+    'Riches','Richter','Riley','Ring','Ripley','Rippy','Rivera','Roberts',
+    'Robertson','Robinson','Rodgers','Rogers','Roseboro','Rosen','Ross',
+    'Rouse','Royal','Rucker','Russell','Saunders','Sawyer','Scott','Seawell',
+    'Shannon','Sharples','Shaw','Shelton','Shoffner','Short','Showfety',
+    'Simmons','Sims','Singer','Small','Smith','Snyder','Southern','Spencer',
+    'Staley','Stanley','Stephens','Stephenson','Stevens','Stewart','Stone',
+    'Stricklen','Swann','Sumner','Swaney','Talbert','Tate','Taylor','Thomas',
+    'Thompson','Thorne','Townsend','Troxler','Turner','Tuttle','Tyson',
+    'Umfleet','Underwood','Utley','Vance','Vanstory','Vines','Vaughan',
+    'Vestall','Walker','Wall','Wallace','Walters','Ward','Warren','Washington',
+    'Watkins','Watson','Wayne','Weaver','Webb','Webster','Wells','West',
+    'White','Wilkins','Williams','Wilson','Young','Zimmerman']
+streets = [
+    'River St.','Lindley Ave.','Ray St.','Chruch St.','Carriage Ln.',
+    'Ludwick Ln.','Quate Dr.','Summit Av.','Greenbrair Rd.','Olive St.',
+    'Pinelake Dr.','Orchard St.','Smokeridge Ln.','Coleridge Dr.',
+    'Branson Rd.','Cardigan St.','Main St.','Cottage Av.',
+    'Mr. Luther King Dr.','Westworth Dr.','Hillsdale St.','New Garden Rd.',
+    'Oak Ridge Rd.','Cascade Ln.','Lakebend Way','Deerwood Dr.',
+    'Northwood Ave.','Woodstream Rd.','Leawood Dr.','Grayston Ln.','Knox Rd.',
+    'Bears Creek Rd.','Simons Sq.','Washington St.','Market St.','Country Dr.',
+    'Grove Ln.','Hilltop Rd.','Sherwood St.','Folger Rd.','Company Mill Ln.',
+    'Nelson St.','Hedrick Dr.','Cottage Pl.','United St.','Hewitt St.',
+    'Walker Ave.','Burns St.','Raintree Ln.','Juliet Dr.','Garlston Dr.',
+    'Kingston St.','Elm St.','Quaker Ave.','Rollingwood Dr.','Tower Rd.',
+    'Lakepoint Dr.','Chapman St.','Boyd Rd.','Ross Av.','Dogwood Dr.',
+    'Fisher Park Pl.','Murphy Rd.','Summertree Ave.','Bluemont Dr.',
+    'Weslo St.','Flint St.','Stillwell Rd.','Bingham St.','Kenview Ave.']
+d = zlib.decompress(open('zipdb.bin','rb').read()).split(b'\x0a')
+sr = random.SystemRandom()
+ppl_id = sr.randrange(maxpop)
+ppl_sum = 0
+for line in d:
+    p=line.find(b';')+1
+    ppl_sum += int(line[p:line.find(b';',p)])
+    if ppl_sum>ppl_id:
+        s = line.decode('cp1252')
+        break
+s = s.split(';')
+ZipCode = s[0]
+MalePopulation = int(s[2])
+FemalePopulation = int(s[3])
+isMale = sr.randrange(MalePopulation + FemalePopulation) < MalePopulation
+if isMale:
+    Age = float(s[4])
+else:
+    Age = float(s[5])
+Latitude = float(s[6])
+Longitude = float(s[7])
+Elevation = int(s[8])
+State = s[9]
+StateFullName = s[10]
+CityAliasAbbreviation = s[11]
+AreaCode = sr.choice(s[12].split('/'))
+City = s[13].title()
+CountyName = s[14].title()
+CountyFIPS = s[15]
+StateFIPS = s[16]
+TimeZone = s[17]
+DayLightSaving = s[18]
+CBSA_Name = s[19]
+Region = s[20]
+Division = s[21]
+PreferredLastLineKey = s[22]
+CBSA_Div_Name = s[23]
+FirstName = sr.choice(man_names) if isMale else sr.choice(woman_names)
+LastName = sr.choice(surnames)
+Street = sr.choice(streets)
+Phone = AreaCode + '-' + str(sr.randrange(200, 970+1)) + '-' + str(sr.randrange(1000, 9990+1))
+Address = str(sr.randrange(50, 3000+1)) + ' ' + Street
+print(FirstName + ' ' + LastName)
+print(Address)
+print(City + ', ' + State + ' ' + ZipCode + '\n')
+print('--- Additional info ---')
+print('First Name:           ' + FirstName)
+print('Last Name:            ' + LastName)
+print('Sex:                  ' + ('Male' if isMale else 'Female'))
+print('Age:                  ' + str(Age))
+print('Estimated D.O.B.:     ' + str(datetime.date.fromordinal(round(datetime.date.today().toordinal()-365.25*Age))))
+print('Address:              ' + Address)
+print('City:                 ' + City)
+print('State:                ' + State + ' (' + StateFullName + ')')
+print('Zip code:             ' + ZipCode)
+print('Phone number:         ' + Phone)
+print('Latitude:             ' + str(Latitude))
+print('Longitude:            ' + str(Longitude))
+print('Elevation:            ' + str(Elevation) + ' m')
+print('Time zone:            ' + TimeZone)
+print('Daylight saving:      ' + DayLightSaving)
+if CityAliasAbbreviation:
+    print('City abbr.:           ' + CityAliasAbbreviation)
+print('County name:          ' + CountyName)
+print('County FIPS:          ' + CountyFIPS)
+print('State FIPS:           ' + StateFIPS)
+if Region:
+    print('Region:               ' + Region)
+if Division:
+    print('Division:             ' + Division)
+if CBSA_Name:
+    print('CBSA name:            ' + CBSA_Name)
+if CBSA_Div_Name:
+    print('CBSA division name:   ' + CBSA_Div_Name)
+print('PreferredLastLineKey: ' + PreferredLastLineKey)
---
+++ idgen_rus.py
@@ -0,0 +1,187 @@
+﻿import zlib
+import random
+
+maxmalefam = 6497311
+maxfemfam = 6911258
+maxmalename = 8632824
+maxfemname = 8496163
+maxmaleotc = 8561810
+maxfemotc = 8492512
+adr2 = (
+(0x000000, 0x0340C),
+(0x00340C, 0x21055),
+(0x024461, 0x09048),
+(0x02D4A9, 0x01C80),
+(0x02F129, 0x06D65),
+(0x035E8E, 0x00251),
+(0x0360DF, 0x042D9),
+(0x03A3B8, 0x00904),
+(0x03ACBC, 0x033AC),
+(0x03E068, 0x06B28),
+(0x044B90, 0x06AC7),
+(0x04B657, 0x0589E),
+(0x050EF5, 0x05C03),
+(0x056AF8, 0x0767A),
+(0x05E172, 0x0313C),
+(0x0612AE, 0x12DD9),
+(0x074087, 0x02FF1),
+(0x077078, 0x0F444),
+(0x0864BC, 0x04429),
+(0x08A8E5, 0x049EA),
+(0x08F2CF, 0x09ED8),
+(0x0991A7, 0x12B73),
+(0x0ABD1A, 0x1EF50),
+(0x0CAC6A, 0x15439),
+(0x0E00A3, 0x0DC4F),
+(0x0EDCF2, 0x0F984),
+(0x0FD676, 0x0873F),
+(0x105DB5, 0x06F2C),
+(0x10CCE1, 0x06A1B),
+(0x1136FC, 0x09299),
+(0x11C995, 0x0AC72),
+(0x127607, 0x0FCDB),
+(0x1372E2, 0x0CDBB),
+(0x14409D, 0x14A4A),
+(0x158AE7, 0x10D43),
+(0x16982A, 0x1A4A3),
+(0x183CCD, 0x0E843),
+(0x192510, 0x11E64),
+(0x1A4374, 0x078A9),
+(0x1ABC1D, 0x06687),
+(0x1B22A4, 0x01D28),
+(0x1B3FCC, 0x1795F),
+(0x1CB92B, 0x15E66),
+(0x1E1791, 0x0A20E),
+(0x1EB99F, 0x024CB),
+(0x1EDE6A, 0x08C8B),
+(0x1F6AF5, 0x093D9),
+(0x1FFECE, 0x08344),
+(0x208212, 0x01947),
+(0x209B59, 0x2601D),
+(0x22FB76, 0x024A9),
+(0x23201F, 0x1EF58),
+(0x250F77, 0x0C52A),
+(0x25D4A1, 0x100E8),
+(0x26D589, 0x11403),
+(0x27E98C, 0x1111D),
+(0x28FAA9, 0x07CDD),
+(0x297786, 0x0BEE3),
+(0x2A3669, 0x150D2),
+(0x2B873B, 0x0EF67),
+(0x2C76A2, 0x27207),
+(0x2EE8A9, 0x0A1E6),
+(0x2F8A8F, 0x0BA08),
+(0x304497, 0x16AE7),
+(0x31AF7E, 0x04559),
+(0x31F4D7, 0x188EC),
+(0x337DC3, 0x079DA),
+(0x33F79D, 0x0B506),
+(0x34ACA3, 0x18494),
+(0x363137, 0x08178),
+(0x36B2AF, 0x0E435),
+(0x3796E4, 0x0BAD6),
+(0x3851BA, 0x0A541),
+(0x38F6FB, 0x1A940),
+(0x3AA03B, 0x0A6C7),
+(0x3B4702, 0x0F4FF),
+(0x3C3C01, 0x10216),
+(0x3D3E17, 0x0AAC9),
+(0x3DE8E0, 0x020A7),
+(0x3E0987, 0x001B0),
+(0x3E0B37, 0x01ECD),
+(0x3E2A04, 0x00650),
+(0x3E3054, 0x003B1),
+(0x3E3405, 0x00495),
+(0x3E389A, 0x01A4D),
+(0x3E52E7, 0x0438C),
+(0x3E9673, 0x0100E),
+(0x3EA681, 0x0013C),
+(0x3EA7BD, 0x024DB),
+(0x3ECC98, 0x0028C))
+
+sr = random.SystemRandom()
+sex = sr.randrange(2)
+if sex:
+    d = zlib.decompress(open('malefam.bin','rb').read()).split(b'\x0a')
+    fid = sr.randrange(maxmalefam)
+else:
+    d = zlib.decompress(open('femfam.bin','rb').read()).split(b'\x0a')
+    fid = sr.randrange(maxfemfam)
+id_sum = 0
+for line in d:
+    id_sum += int(line[:line.find(b';')])
+    if id_sum>fid:
+        s = line.decode('cp866')
+        break
+Fam = s[s.find(';')+1:].title()
+if sex:
+    d = zlib.decompress(open('malename.bin','rb').read()).split(b'\x0a')
+    fid = sr.randrange(maxmalename)
+else:
+    d = zlib.decompress(open('femname.bin','rb').read()).split(b'\x0a')
+    fid = sr.randrange(maxfemname)
+id_sum = 0
+for line in d:
+    id_sum += int(line[:line.find(b';')])
+    if id_sum>fid:
+        s = line.decode('cp866')
+        break
+Name = s[s.find(';')+1:].title()
+if sex:
+    d = zlib.decompress(open('maleotc.bin','rb').read()).split(b'\x0a')
+    fid = sr.randrange(maxmaleotc)
+else:
+    d = zlib.decompress(open('femotc.bin','rb').read()).split(b'\x0a')
+    fid = sr.randrange(maxfemotc)
+id_sum = 0
+for line in d:
+    id_sum += int(line[:line.find(b';')])
+    if id_sum>fid:
+        s = line.decode('cp866')
+        break
+Otc = s[s.find(';')+1:].title()
+print(Fam, Name, Otc)
+d = zlib.decompress(open('adr1.bin','rb').read()).split(b'\x0a')
+fid = sr.randrange(142369610)
+id_sum = 0
+i = 0
+for line in d:
+    id_sum += int(line[:line.find(b';')])
+    if id_sum>fid:
+        s = line.decode('cp866')
+        break
+    else:
+        i += 1
+FullAdr = s[s.find(';')+1:]
+f = open('adr2.bin','rb')
+f.seek(adr2[i][0])
+d = zlib.decompress(f.read(adr2[i][1])).split(b'\x0a')
+f.close()
+i = 0
+while True:
+    numread = int(d[i])
+    i += 1
+    sl = []
+    sumi = 0
+    for j in range(numread):
+        s = d[i]
+        sumi += int(s[:s.find(b';')])
+        sl.append(s)
+        i += 1
+    iid = sr.randrange(sumi)
+    sumi = 0
+    for line in sl:
+        sumi += int(line[:line.find(b';')])
+        if sumi>iid:
+            s = line.split(b';')
+            break
+    FullAdr += ' -> '+s[3].decode('cp866')
+    if s[1]:
+        Index = s[1].decode('cp866')
+    if not s[2]:
+        break
+    while d[i] != s[2]:
+        i += 1
+    i += 1
+print(Index)
+print(FullAdr)
